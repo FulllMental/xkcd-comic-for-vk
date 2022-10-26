2e1568feab3064ddf306dc7181ce3f6fcc535098
FulllMental <104234625+FulllMental@users.noreply.github.com>
2022-10-26 15:58:47 +0300

    Add VK API auth script

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import requests
 import logging
+import os
+from dotenv import load_dotenv
 from os.path import splitext
 from urllib.parse import unquote, urlsplit
+
+
+def get_vk_response(access_vk_token, group_id):
+    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+    payload = {
+        'access_token': access_vk_token,
+        'v': 5.131,
+        'group_id': group_id,
+    }
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()
 
 
 def get_xkcd_answer():
@@ -33,10 +47,20 @@ def download_comic(xkcd_response):
 
 
 if __name__ == '__main__':
+    load_dotenv()
+    access_vk_token = os.getenv('ACCESS_VK_TOKEN')
+    group_id = os.getenv('GROUP_ID')
     logging.basicConfig(level=logging.DEBUG)
-    logging.info('Получение ответа от xkcd...')
 
-    xkcd_response = get_xkcd_answer()
-    download_comic(xkcd_response)
-    xkcd_comment = xkcd_response['alt']
-    print(xkcd_comment)
+    # logging.info('Получение ответа от xkcd...')
+    #
+    # xkcd_response = get_xkcd_answer()
+    # # download_comic(xkcd_response)
+    #
+    # logging.info('Получение комментария к картинке xkcd...')
+    # xkcd_comment = xkcd_response['alt']
+    # print(xkcd_comment)
+
+    vk_response = get_vk_response(access_vk_token, group_id)
+    print(vk_response)
+
