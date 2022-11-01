011c9260fa15c3a2a34cb93db8054416e94ac47c
FulllMental <fulllmental@gmail.com>
2022-11-01 12:40:47 +0300

    Change token naming

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import requests
 from dotenv import load_dotenv
 
 
-def upload_vk_picture(access_vk_token, group_id, filename):
+def upload_vk_picture(vk_access_token, vk_group_id, filename):
 
     logging.info('Загрузка картинки на сервер VK...')
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     payload = {
-        'access_token': access_vk_token,
+        'access_token': vk_access_token,
         'v': 5.131,
-        'group_id': group_id,
+        'group_id': vk_group_id,
     }
     response = requests.get(url, params=payload)
     response.raise_for_status()
@@ -29,14 +29,14 @@ def upload_vk_picture(access_vk_token, group_id, filename):
     return response.json()
 
 
-def save_vk_picture(access_vk_token, group_id, photo, server, vk_hash):
+def save_vk_picture(vk_access_token, vk_group_id, photo, server, vk_hash):
 
     logging.info('Сохранение картинки в альбом...')
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     payload = {
-        'access_token': access_vk_token,
+        'access_token': vk_access_token,
         'v': 5.131,
-        'group_id': group_id,
+        'group_id': vk_group_id,
         'photo': photo,
         'server': server,
         'hash': vk_hash,
@@ -46,15 +46,15 @@ def save_vk_picture(access_vk_token, group_id, photo, server, vk_hash):
     return response.json()
 
 
-def post_vk_picture(access_vk_token, group_id, picture_id, owner_id, xkcd_comment):
+def post_vk_picture(vk_access_token, vk_group_id, picture_id, owner_id, xkcd_comment):
 
     logging.info('Публикую картинку и пост в группу...')
     url = 'https://api.vk.com/method/wall.post'
     payload = {
-        'access_token': access_vk_token,
+        'access_token': vk_access_token,
         'v': 5.131,
-        'group_id': group_id,
-        'owner_id': f'-{group_id}',
+        'group_id': vk_group_id,
+        'owner_id': f'-{vk_group_id}',
         'from_group': 1,
         'attachments': f'photo{owner_id}_{picture_id}',
         'message': xkcd_comment,
@@ -86,7 +86,7 @@ def download_random_xkcd_comic():
     xkcd_comment = random_comic['alt']
     picture_extension = get_picture_extension(image_url)
     filename = f'comic{picture_extension}'
-    
+
     logging.info('Скачивание картинки...')
     response = requests.get(image_url)
     response.raise_for_status()
@@ -97,20 +97,20 @@ def download_random_xkcd_comic():
 
 if __name__ == '__main__':
     load_dotenv()
-    access_vk_token = os.getenv('ACCESS_VK_TOKEN')
-    group_id = os.getenv('GROUP_ID')
+    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
+    vk_group_id = os.getenv('VK_GROUP_ID')
     logging.basicConfig(level=logging.INFO)
     try:
         filename, xkcd_comment = download_random_xkcd_comic()
 
         logging.info('Получение комментария к картинке xkcd...')
-        upload_response = upload_vk_picture(access_vk_token, group_id, filename)
+        upload_response = upload_vk_picture(vk_access_token, vk_group_id, filename)
         photo = upload_response['photo']
         server = upload_response['server']
         vk_hash = upload_response['hash']
-        save_vk_picture_response = save_vk_picture(access_vk_token, group_id, photo, server, vk_hash)
+        save_vk_picture_response = save_vk_picture(vk_access_token, vk_group_id, photo, server, vk_hash)
         picture_id = save_vk_picture_response["response"][0]["id"]
         owner_id = save_vk_picture_response["response"][0]["owner_id"]
-        post_vk_picture(access_vk_token, group_id, picture_id, owner_id, xkcd_comment)
+        post_vk_picture(vk_access_token, vk_group_id, picture_id, owner_id, xkcd_comment)
     finally:
         os.remove(filename)
